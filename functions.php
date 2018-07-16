e70e026cf5dd2f4f3412604dc66109c98922bf8e
Elias Roberts <eliroberts9@gmail.com>
2018-07-16 15:11:58 -0600

    added money_format('%.2n' to all currency types to better represent currency

--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 // Mathematical function
 function feelRich($amount) {
 	// Converts USD to Zimbabwean dollars
-	$zimbabwe = ($amount * 30000);
+	$zimbabwe = money_format('%.2n', ($amount * 30000));
 	// Converts USD to Japanese yen
-	$yen = ($amount * 112.8);
+	$yen = money_format('%.2n', ($amount * 112.8));
 	// Converts USD to Armenian Dram
-	$armenianDram = ($amount * 480.10);
+	$armenianDram = money_format('%.2n', ($amount * 480.10));
 	return "You have: Z\$$zimbabwe Zimbabwean Dollars, ¥$yen Japanese Yen, and 
 	֏$armenianDram Armenian Dram! Of course, this means nothing.";
 }
